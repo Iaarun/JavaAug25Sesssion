1e14b091028a5008c6639571649b4df07b988a06
Iaarun <automationbyaarun@gmail.com>
2025-09-24 07:57:47 +0530

    update for handling inputbox and checkbox

--- src/seleniumdemo/BasicSeleniumFunctions.java
+++ src/seleniumdemo/BasicSeleniumFunctions.java
@@ -1,20 +1,23 @@
 package seleniumdemo;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.List;
 import java.util.Set;
 
 public class BasicSeleniumFunctions {
     WebDriver driver;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         BasicSeleniumFunctions obj = new BasicSeleniumFunctions();
         obj.launchBrowser("chrome");
        // obj.launchApp("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-       obj.browserNavigation();
+       obj.selectMultipleCheckBox();
     //    obj.closeBrowser();
     }
 
@@ -40,6 +43,42 @@ public class BasicSeleniumFunctions {
         driver.get(url);
     }
 
+    public void selectMultipleCheckBox(){
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
+     List<WebElement> boxes= driver.findElements(By.name("my-check"));
+        System.out.println("Number of check boxes: "+boxes.size());
+     boxes.forEach(box->{
+         if(!box.isSelected()){
+             box.click();
+         }
+     });
+
+    }
+
+    public void handleCheckBoxAndRadioButton() throws InterruptedException {
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
+     WebElement check1=   driver.findElement(By.id("my-check-1"));
+     // isSelected method will return true if the element is selected otherwise false
+        System.out.println(check1.isSelected());
+        Thread.sleep(2000);
+        check1.click();
+        System.out.println(check1.isSelected());
+  // Write the code to handle radio button
+    }
+   public void sendDataToInputField(){
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
+   WebElement userNameField= driver.findElement(By.id("my-text-id"));
+   System.out.println(userNameField.isDisplayed());
+   //sendkeys to enter data in the input field
+       userNameField.sendKeys("Hello Selenium");
+     WebElement password=  driver.findElement(By.name("my-password"));
+        password.sendKeys("Selenium123");
+
+        WebElement disableLement= driver.findElement(By.name("my-disabled"));
+        // isEnabled method will return true if the element is enabled otherwise false
+       System.out.println(disableLement.isEnabled());
+      //  disableLement.sendKeys("test");
+   }
     public void browserNavigation(){
         driver.navigate().to("https://bonigarcia.dev/selenium-webdriver-java/");
         System.out.println("Current URL: "+driver.getCurrentUrl()); //bonigarcia
